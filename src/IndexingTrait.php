514703f86c5855803f4bd06b7b99a176d4809100
Rohan <me@rohandey.com>
2021-07-01 22:25:24 +0530

    Update IndexingTrait.php

--- src/IndexingTrait.php
+++ src/IndexingTrait.php
@@ -8,9 +8,6 @@ trait IndexingTrait
 {
     public function geoToH3(float $lat, float $lon, int $res): string
     {
-        if (php_sapi_name() !== 'cli') {
-            return (new \H3())->geoToH3($lat, $lon, $res);
-        }
 
         $ffi = FFI::cdef(
             self::H3IndexTypeDef.self::GeoCoordTypeDef.
@@ -29,9 +26,6 @@ trait IndexingTrait
 
     public function h3ToGeo(string $h3Index): object
     {
-        if (php_sapi_name() !== 'cli') {
-            return (new \H3())->h3ToGeo($h3Index);
-        }
 
         $ffi = FFI::cdef(
             self::H3IndexTypeDef.self::GeoCoordTypeDef.
