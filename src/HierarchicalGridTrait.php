0a1fb33bb94c5779a2595ffdaadc32a0798066b0
Rohan <me@rohandey.com>
2021-07-01 22:24:17 +0530

    Update HierarchicalGridTrait.php

--- src/HierarchicalGridTrait.php
+++ src/HierarchicalGridTrait.php
@@ -8,9 +8,6 @@ trait HierarchicalGridTrait
 {
     public function h3ToParent(string $H3Index, int $parentRes): string
     {
-        if (php_sapi_name() !== 'cli') {
-            return (new \H3())->h3ToParent($H3Index, $parentRes);
-        }
 
         $ffi = FFI::cdef(self::H3IndexTypeDef.'H3Index h3ToParent(H3Index h, int parentRes);', $this->lib);
 
